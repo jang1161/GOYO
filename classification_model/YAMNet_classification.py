76899bac64947244314059d5a5f1470fd9506cd2
kimtaerimm <trnara5375@gmail.com>
2025-10-22 14:03:13 +0900

    AI

--- classification_model/YAMNet_classification.py
+++ classification_model/YAMNet_classification.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
+import librosa
 import csv
 
-from scipy.io import wavfile
-from scipy import signal
+#from scipy.io import wavfile
+#from scipy import signal
 
 # Load the model.
 model = hub.load('./yamnet_1')
@@ -26,24 +27,24 @@ class_names = class_names_from_csv(class_map_path)
 
 
 
-def ensure_sample_rate(original_sample_rate, waveform,
-                       desired_sample_rate=16000):
-  """Resample waveform if required."""
-  if original_sample_rate != desired_sample_rate:
-    desired_length = int(round(float(len(waveform)) /original_sample_rate * desired_sample_rate))
-    waveform = signal.resample(waveform, desired_length)
-  return desired_sample_rate, waveform
+# def ensure_sample_rate(original_sample_rate, waveform,
+#                        desired_sample_rate=16000):
+#   """Resample waveform if required."""
+#   if original_sample_rate != desired_sample_rate:
+#     desired_length = int(round(float(len(waveform)) /original_sample_rate * desired_sample_rate))
+#     waveform = signal.resample(waveform, desired_length)
+#   return desired_sample_rate, waveform
 
 
 
 # wav_file_name = 'speech_whistling2.wav'
 # 이따가 파일이 들어갈 경로를 저기다가 해야함
-wav_file_name = 'miaow_16k.wav'
+wav_file_name = 'vibration.mp3'
 
 try:
     
-    sample_rate, wav_data = wavfile.read(wav_file_name, 'rb')
-    sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
+    wav_data, sample_rate = librosa.load(wav_file_name, sr=16000, mono=True)
+    # sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
 
     # Show some basic information about the audio.
     duration = len(wav_data)/sample_rate
@@ -51,7 +52,11 @@ try:
     print(f'Total duration: {duration:.2f}s')
     print(f'Size of the input: {len(wav_data)}')
 
-    waveform = wav_data / tf.int16.max
+    # if wav_data.ndim != 1:  #dimension 1차원으로 통일?
+    #   wav_data = np.mean(wav_data, axis=1)
+      
+    waveform = wav_data
+    waveform = tf.cast(waveform, tf.float32)
 
     # Run the model, check the output.
     scores, embeddings, spectrogram = model(waveform)
